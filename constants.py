311f34883d134fb0002e5644f609fb7468b71c57
Ubuntu <ubuntu@ip-172-31-24-253.ec2.internal>
2019-10-16 23:19:29 +0000

    added program

---
+++ constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Davin Rousseau
+# Created on September 2019
+# This module contains constants for sprite border program
+
+# Circuitpython screen size is 160x120 and sprites are 16x16
+
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
